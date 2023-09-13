27ad8733fd3cb4397e0ed4d5a66489ba81b51463
Alexgithubnw <141021995+Alexgithubnw@users.noreply.github.com>
2023-09-13 13:16:29 +0300

    addet project

---
+++ src/js/add.js
@@ -0,0 +1,28 @@
+function showMenu(){
+	console.log('hi')
+}
+const swiper = new Swiper('.image-slider', {
+    navigation: {
+        nextEl: '.swiper-button-next',
+		prevEl: '.swiper-button-prev'
+    },
+	pagination: {
+		el: '.swiper-pagination',
+		clickable: true,
+	},
+	slideToClickedSlide: true,
+	keyboard: {
+		enable: true,
+		onlyInViewport: true,
+		pageUpDown: true,
+	},
+	mousewheel: { 
+		sensitivity: 1,
+	},
+	autoHeight: true,
+    speed: 800,
+    effect: 'fade',
+    fadeEffect:{
+        crossFade: true,
+    },
+});
